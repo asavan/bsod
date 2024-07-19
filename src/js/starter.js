19a7e1e488a3af46c54442fcc0753350cadcc454
a.savanovich <asavanster@gmail.com>
2024-07-19 17:52:09 +0300

    fix

--- src/js/starter.js
+++ src/js/starter.js
@@ -2,7 +2,7 @@ import settingsOriginal from "./settings.js";
 import { parseSettings, adjustSeed } from "./utils/parse-settings.js";
 import rngFunc from "./utils/random.js";
 
-export default function starter(window, document) {
+export default function starter(window) {
     const settings = { ...settingsOriginal };
     const changed = parseSettings(window.location.search, settings);
     const rngEngine = Math.random;
