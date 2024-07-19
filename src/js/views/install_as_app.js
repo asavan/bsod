f932f5f624e112a53ed3bf0e875c996bdf526605
a.savanovich <asavanster@gmail.com>
2024-07-19 21:12:59 +0300

    return click

--- src/js/views/install_as_app.js
+++ src/js/views/install_as_app.js
@@ -5,21 +5,22 @@ export default function install(window, document) {
         return;
     }
     let deferredPrompt;
-    btnAdd.addEventListener("click", (e) => {
-        e.preventDefault();
-        // Show the prompt
-        deferredPrompt.prompt();
-        // Wait for the user to respond to the prompt
-        deferredPrompt.userChoice.then((resp) => {
-            console.log(JSON.stringify(resp));
-        });
-    });
 
     window.addEventListener("beforeinstallprompt", (e) => {
         // Prevent the mini-info bar from appearing.
         e.preventDefault();
         // Stash the event so it can be triggered later.
         deferredPrompt = e;
+
+        btnAdd.addEventListener("click", (e) => {
+            e.preventDefault();
+            // Show the prompt
+            deferredPrompt.prompt();
+            // Wait for the user to respond to the prompt
+            deferredPrompt.userChoice.then((resp) => {
+                console.log(JSON.stringify(resp));
+            });
+        });    
     });
     return btnAdd;
 }
