fcf5ab49be305723c9059e460de20a1917f3add8
a.savanovich <asavanster@gmail.com>
2024-10-18 18:21:37 +0300

    fix typos

--- src/index.js
+++ src/index.js
@@ -1,42 +1,12 @@
-import install from "./js/views/install_as_app.js";
-
-resize();
-function resize() {
-    //resize check
-    const edit = document.getElementById("edit");
-    if (!edit) {
-        return false;
-    }
-    const masterTable = document.getElementById("masterTable");
-
-    if (Math.abs(window.innerHeight - screen.height) < 5) {
-        //disable editing
-        edit.contentEditable = "false";
-        masterTable.style.cursor = "none";
-        deleteSelection();
-    } else {
-        edit.contentEditable = "true";
-        masterTable.style.cursor = "";
-    }
-}
-
-function deleteSelection() {
-    if (window.getSelection) {
-        // Mozilla
-        const selection = window.getSelection();
-        if (selection.rangeCount > 0) {
-            window.getSelection().removeAllRanges();
-        }
-    }
-}
+import install from "./js/views/install-as-app.js";
 
 //GSOD support
 {
     const green = new URLSearchParams(window.location.search).has("g");
     if (green) {
-        document.getElementById("body").style.backgroundColor = "#3A6D2F";
-        document.getElementById("qr").style.filter = "hue-rotate(262deg)";
-        document.getElementById("devicename").innerHTML = "Windows Insider Build";
+        document.querySelector("#body").style.backgroundColor = "#3A6D2F";
+        document.querySelector("#qr").style.filter = "hue-rotate(262deg)";
+        document.querySelector("#devicename").innerHTML = "Windows Insider Build";
     }
 }
 
